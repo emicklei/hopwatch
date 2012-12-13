c00817ee1b63109795662efcdde04a24e5b7cdf0
Ernest Micklei <ernest.micklei@gmail.com>
2012-12-13 23:48:01 +0100

    package comment only once

--- hopwatch.go
+++ hopwatch.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// Hopwatch is a debugging tool for Go programs.
 package hopwatch
 
 import (
